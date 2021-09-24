970599e0597e758d548a287e7a9e6ce6d52269f5
meatBox <jinwoo.park@meatBoxui-MacBookPro.local>
2021-09-24 14:02:53 +0900

    added: RTK with sample components

---
+++ src/components/Message.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet} from 'react-native';
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../store';
+import { setMessage } from '../store/message';
+const Message = () => {
+    const dispatch = useDispatch();
+    const { message }= useSelector((state: RootState) => state.message);
+
+    const handlePress = () => {
+        dispatch(setMessage('Message from Component'));
+    };
+
+
+    return (
+     <View style={styles.container}>
+         <Text style={styles.text}>
+                { message}
+         </Text>
+         <Text> RTK </Text>
+         <Button title={"Set Message"} onPress={handlePress}/>
+     </View>
+    )
+}
+
+export default Message;
+
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    text: {
+      fontSize: 18
+    }
+  });
+  
+
